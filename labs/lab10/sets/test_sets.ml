ce05f071d2d8be046777ea4d2552fcf4c275321a
Zhe Wang <wangzhetju@gmail.com>
2020-11-02 22:33:16 +0000

    lab9

--- labs/lab10/sets/test_sets.ml
+++ labs/lab10/sets/test_sets.ml
@@ -4,6 +4,8 @@ open Sets.ListSet
 let tests = "test suite for ListSet" >::: [
   "test_empty" >:: (fun _ -> assert_equal [] (to_list empty));
   (* add more black box tests here *)
+  "test_size_of_empty" >:: (fun _ -> assert_equal 0 (size empty))
+  "test_size_insert" >:: (fun _ -> 1 (insert 1 empty))
 ]
 
 let _ = run_test_tt_main tests
